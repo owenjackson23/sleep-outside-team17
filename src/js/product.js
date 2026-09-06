37c96e856e9f273704cfa61b4adac9559e1a8980
owenjackson23 <owenryanjackson@icloud.com>
2026-09-05 18:47:05 -0700

    debugged addProductToCart() function

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get the current cart from local storage
+  const currentCart = getLocalStorage("so-cart") || [];
+  // Add the product to the current cart
+  currentCart.push(product);
+  // Update local storage with the new cart
+  setLocalStorage("so-cart", currentCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
