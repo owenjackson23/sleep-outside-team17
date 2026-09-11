dd768ab9a09c62f2e7e8f93ae832463804862455
owenjackson23 <owenryanjackson@icloud.com>
2026-09-11 15:49:31 -0700

    added getParam() function get a query string parameter from the URL

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,12 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+
+// gets a query string parameter from the URL
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get('product');
+  return product;
+}
