f67fb7247fcea738fd571d1988834f3c45fc8337
owenjackson23 <owenryanjackson@icloud.com>
2026-09-11 22:34:44 -0700

    created an instance of productList

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,9 @@ import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 
 const dataSource = new ProductData('tents');
+
+const listElement = document.querySelector('.product-list');
+
+const productList = new ProductList('tent', 'dataSource', listElement);
+
+productList.init();
