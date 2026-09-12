55778dbac5e548047ddee7c282481efdb8071ff3
owenjackson23 <owenryanjackson@icloud.com>
2026-09-11 18:23:09 -0700

    implemented init() method

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,3 +1,5 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
 export default class ProductDetails {
     constructor(productId, dataSource) {
         this.productId = productId;
@@ -5,7 +7,16 @@ export default class ProductDetails {
         this.dataSource = dataSource;
     }
 
-    init() { }
+    async init() {
+        // use the datasource to get the details for the current product
+        this.product = await this.dataSource.findProductById(this.productId);
+        // the product details are needed before rendering the HTML
+        this.renderProductDetails();
+        // once the HTML is rendered, add a listener to the Add to Cart button
+        // Notice the .bind(this). This callback will not work if the bind(this) is missing. Review the readings from this week on 'this' to understand why.
+        document.getElementById('addToCart')
+            .addEventListener('click', this.addProductToCart.bind(this));
+    }
 
     addProductToCart(product) {
         // Get the current cart from local storage
