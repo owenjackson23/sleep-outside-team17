0ec1aca6f66f6ef69af458411e99ec917a86d90c
owenjackson23 <owenryanjackson@icloud.com>
2026-09-11 17:09:59 -0700

    moved constructor, addProductToCart, and renderProductDetails inside of the ProductDetails class

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,14 +1,20 @@
-//constructor() { }
+export default class ProductDetails {
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
 
-//init() { }
+    //init() { }
 
-function addProductToCart(product) {
-    // Get the current cart from local storage
-    const currentCart = getLocalStorage("so-cart") || [];
-    // Add the product to the current cart
-    currentCart.push(product);
-    // Update local storage with the new cart
-    setLocalStorage("so-cart", currentCart);
+    addProductToCart(product) {
+        // Get the current cart from local storage
+        const currentCart = getLocalStorage("so-cart") || [];
+        // Add the product to the current cart
+        currentCart.push(product);
+        // Update local storage with the new cart
+        setLocalStorage("so-cart", currentCart);
+    }
+
+    renderProductDetails() { }
 }
-
-function renderProductDetails() { }
