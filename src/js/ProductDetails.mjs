9abb2232cdf6e61428ed1fbdb5ac032abc3a9fcb
owenjackson23 <owenryanjackson@icloud.com>
2026-09-11 18:55:31 -0700

    created productDetailsTemplate() function and implemented renderProductDetails()

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -28,6 +28,30 @@ export default class ProductDetails {
     }
 
     renderProductDetails() {
-        document.querySelector('h3').innerHTML = this.product.NameWithoutBrand;
+        productDetailsTemplate(this.product);
     }
+}
+
+function productDetailsTemplate(product) {
+    return `<section class="product-detail">
+            <h3>${product.Brand.Name}</h3>
+
+            <h2 class="divider">${product.NameWithoutBrand}</h2>
+
+            <img class="divider"
+                src="${product.Image}"
+                alt="${product.NameWithoutBrand}" />
+
+            <p class="product-card__price">$${product.FinalPrice}</p>
+
+            <p class="product__color">${product.Colors[0].ColorName}</p>
+
+            <p class="product__description">
+                ${product.DescriptionHtmlSimple}
+            </p>
+
+            <div class="product-detail__add">
+                <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+            </div>
+        </section>`;
 }
