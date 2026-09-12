d59211abb3b1ff455e4adeaad50c458193315cfb
owenjackson23 <owenryanjackson@icloud.com>
2026-09-11 23:47:19 -0700

    lint, format, removed addToCartHandler and listener in product.js, renderListWithTemplate function implemented

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
-const listElement = document.querySelector('.product-list');
+const listElement = document.querySelector(".product-list");
 
-const productList = new ProductList('tent', 'dataSource', listElement);
+const productList = new ProductList("tent", "dataSource", listElement);
 
 productList.init();
