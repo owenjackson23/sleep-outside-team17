aa19cbafbaa263e3b307724a218a43e7e682b724
owenjackson23 <owenryanjackson@icloud.com>
2026-09-22 13:18:40 -0700

    removed quotes from dataSource in ProductList call

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,7 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-
 const listElement = document.querySelector(".product-list");
-
-const productList = new ProductList("tent", "dataSource", listElement);
+const productList = new ProductList("tent", dataSource, listElement);
 
 productList.init();
