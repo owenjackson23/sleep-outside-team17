274acd3f9dc8fefdc16f12cc5ad302db4231d0bd
owenjackson23 <owenryanjackson@icloud.com>
2026-09-22 12:35:55 -0700

    corrected ProductData import statement

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,5 +1,5 @@
 import { renderListWithTemplate } from "./utils.mjs";
-import { ProductData } from "./ProductData.mjs";
+import ProductData from "./ProductData.mjs";
 
 function productCardTemplate(product) {
     return `<li class="product-card">
