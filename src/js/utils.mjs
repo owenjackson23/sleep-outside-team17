90acff7353511e5010ba5ea8c96530aecb5ad482
owenjackson23 <owenryanjackson@icloud.com>
2026-09-24 13:22:01 -0700

    changed templateFn parameter to template in renderListWithTemplate

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -32,8 +32,8 @@ export function getParam(param) {
 }
 
 
-export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
-  const htmlStrings = list.map(templateFn);
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
   if (clear) {
     parentElement.innerHTML = "";
   }
